83c209544344737ecc6066f44e52903c39e556ef
Gaayathiri <gaayathirigeetha@gmail.com>
2024-11-26 08:46:59 -0500

    C2Fconverter

---
+++ C2Fconverter.js
@@ -0,0 +1,10 @@
+function convert()
+{
+    const celsiusValue = Number(document.getElementById("input").value);
+    let farenheitValue = (celsiusValue*1.8)+32;
+    const result = document.getElementById("result");
+    result.innerHTML = farenheitValue + "F"
+}
+    const myname = "Gaayathiri";
+    console.log(myname[Math.floor(Math.random()*myname.length)]);
+
